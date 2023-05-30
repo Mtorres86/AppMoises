6b313d7aacf080a9083a21e5648a348f23d5120d
Mtorres86 <iata24@outlook.es>
2023-05-30 17:31:02 +0200

    se agrego codigo y se probo su ejecucion

--- app/src/main/java/hn/uth/appmoises/MainActivity.java
+++ app/src/main/java/hn/uth/appmoises/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import hn.uth.appmoises.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    //Intanciamos aqui instanciamos nuestro layout y se lo asigamos a una variable para poder aprovecar los recursos que hemos declardo en el layout
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //a partir de aqui podemos acceder a los objetos creados en el lyout
+        binding.btnSaludar.setOnClickListener(v ->{
+            binding.txtTitulo.setText("Bienvenido "+ binding.edtNombre.getText());
+        });
+
     }
 }
